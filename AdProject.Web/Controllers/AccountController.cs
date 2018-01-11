e441b82ee7d887d216014816e388c5f49af35bf6
Cesar Mendes da Silva <cesar.mendes.silva@gmail.com>
2018-01-11 21:37:01 -0200

    desenvolvimento login e registro

---
+++ AdProject.Web/Controllers/AccountController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AdProject.Web.Models;
+
+namespace AdProject.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+            }
+
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+            }
+
+            return View();
+        }
+    }
+}
